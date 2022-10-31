284ec674054f63faa54ed22b2fb4edc41d72274b
Niepep <niepep72@gmail.com>
2022-10-31 20:09:38 +0700

    Initial commit

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,12 @@
+"""
+Program pengulangan membaca buku
+"""
+jumlah_buku = 69
+print('Ibu berkata, "Baca semua buku mu anjeng"')
+jumlah_buku_yang_dibaca = 0
+
+for jumlah_buku_yang_dibaca in range (1,70):
+    print(f"Buku ke {jumlah_buku_yang_dibaca} sudah dibaca")
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}.")
+
+
