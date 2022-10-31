54e9c4ccd9be9b74cf7f57a90819ad272a691fa0
Niepep <niepep72@gmail.com>
2022-10-31 13:43:34 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # Percabangan
 milk_bottle_count = 169
+egg_count= 7
+print(f"Jumlah yang tersedia di toko yaitu {milk_bottle_count} btl susu dan {egg_count} btr telur")
 if milk_bottle_count >0:
+    print("Budi mengecek barang di toko")
     print("Budi megeluarkan uang dan melemparnya")
     print("Budi membeli 1 botol susu")
+    print("Apakah ada telur")
+    if egg_count ==0:
+        print("Budi tidak jadi membeli apapun dan membakar toko tersebut")
+    else:
+        print("Budi membeli 6 butir telur")
 else:
     print("Budi tidak jadi membeli 1 botol susu")
-print("Budi pulang ke rumah")
+print("Budi pulang ke rumah ")
 print("Budi menyerahkan hasilnya kepada ibu")
