aaeb007c6b90a643f24f8077d3274b826168cf36
Niepep <niepep72@gmail.com>
2022-10-31 08:22:54 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Percabangan
+milk_bottle_count = 169
+if milk_bottle_count >0:
+    print("Budi megeluarkan uang dan melemparnya")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+print("Budi pulang ke rumah")
+print("Budi menyerahkan hasilnya kepada ibu")
