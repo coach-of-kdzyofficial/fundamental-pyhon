b50a690373c2147dc4f01a53d0ca4cbea8cba07f
Niepep <niepep72@gmail.com>
2022-11-02 20:59:08 +0700

    perulangan dengan while dengan variabel henti

---
+++ nyoba cukk.py
@@ -0,0 +1,10 @@
+jumlah_buku = 89
+print('Ibu berkata "Baca bukumu anjeng"')
+
+jumlah_buku_sudah_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca sebanyak {jumlah_buku_sudah_dibaca}")
+
+while jumlah_buku_sudah_dibaca < jumlah_buku:
+    jumlah_buku_sudah_dibaca = jumlah_buku_sudah_dibaca + 1
+    print(f"Buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
+print(f"Saya sudah baca {jumlah_buku_sudah_dibaca} buku njenngg")
